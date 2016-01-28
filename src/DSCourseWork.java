eb5d0ac3454894929c59b19743ebc2f6a72a3772
Sheriffo Ceesay <sneceesay77@gmail.com>
2016-01-28 17:51:26 +0000

    Making it dynamic

--- src/DSCourseWork.java
+++ src/DSCourseWork.java
@@ -21,6 +21,9 @@ public class DSCourseWork {
 	HashMap<String, String> fieldsNameMap = new HashMap<String, String>();
 	ArrayList<String> nilFields = new ArrayList<String>();
 	
+	ArrayList<String> keys = null;
+	ArrayList<String> schemaList = null;
+	
 	public static void main(String args[]) {
 		DSCourseWork cw = new DSCourseWork();
 		String database = "protocol.db";
@@ -70,11 +73,13 @@ public class DSCourseWork {
 	public void setupDBandTables(String fileName, Connection cn) {
 		Scanner in;
 		Statement st = null;
-		ArrayList<String> keys = new ArrayList<String>();
+		keys = new ArrayList<String>();
+		schemaList = new ArrayList<String>();
 
 		try {
 			in = new Scanner(new File(fileName));
-			int idPos = 0;
+			int itrCount = 0;
+			int linecount = 0;
 			while (in.hasNextLine()) {
 
 				String line = in.nextLine();
@@ -94,45 +99,45 @@ public class DSCourseWork {
 				
 				//In protocol.txt file, all declarations starting with upper-case 
 				//letters and are found the key declarations represents table names 
-				if (Character.isUpperCase(tokens[0].charAt(0))) {
+				if (linecount > 1) {
+					schemaList.add(tokens[0]);
 					//printLog("Debug", keys.size()+"");
 					// Create Table with columns using tokens[0]
 					String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS "
 							+ tokens[0];
-					StringBuilder sb = new StringBuilder();
-					StringBuilder fieldList = new StringBuilder();
-					int fieldsCount = 0;
+					StringBuilder createTable = new StringBuilder();
 					//Setup up keys int the SQL statement first
 					//for(int i = 0; i<keys.size(); i++){
-					if(keys.size() > 1 && idPos < keys.size()-1){
-						sb.append(keys.get(idPos)+" TEXT PRIMARY KEY NOT NULL,");
-						idPos++;//increment to get the id for next id of next table. 
-						//Order of id list and Message declaration has to be the same. 
-					}else{//Just one key
-						sb.append(keys.get(idPos)+" TEXT PRIMARY KEY NOT NULL,");
+					if(keys.size() == 1 || itrCount == 0){
+						System.out.println("Here 0.0");
+						createTable.append(keys.get(0)+" TEXT PRIMARY KEY NOT NULL,");
+						itrCount++;
+					}else if(keys.size() > 1 && itrCount > 0){
+						System.out.println("Here 1");
+						//Second iteration get first and second
+						createTable.append(keys.get(1)+" TEXT PRIMARY KEY NOT NULL,").append(keys.get(0) + " TEXT  NOT NULL,\n");
+						//increment to get the id for next id of next table. 
+						//Order of id list and Message declaration has to be the same.
 					}
 					//}
 					for (int i = 1; i < tokens.length; i++) {
 						
 						if (i % 2 != 0) {//odd positions have the fields
-							if(!keys.contains(tokens[i]) && !tokens[i+1].equals("nil")){//ignore the keys in this section because they were added in the first loop
-								sb.append(tokens[i] + " TEXT  NOT NULL,\n");
+							//If the field is not a key and not a NIL mapping then append it to create 
+							//table string.
+							if((!keys.contains(tokens[i])) && !tokens[i+1].equals("nil")){
+								createTable.append(tokens[i] + " TEXT  NOT NULL,\n");
 							}else if(tokens[i+1].equals("nil")){//populate the nill list
 								//System.out.println(tokens[0]+""+tokens[i]);
 								nilFields.add(tokens[0]+""+tokens[i]);
 							}
-							fieldsCount++;
-							fieldList.append(tokens[i]+",");
 						}
 					}
-					fieldsCountMap.put(tokens[0], fieldsCount);//Using SQL to handle this
-					fieldsNameMap.put(tokens[0], fieldList.toString());
-					// Create Statement
 					try {
 						// Using substring remove , and \n at the end of the
 						// string
 						sql = CREATE_TABLE + " ( "
-								+ sb.toString().substring(0, sb.length() - 2)
+								+ createTable.toString().substring(0, createTable.length() - 2)
 								+ ");";
 
 						
@@ -145,7 +150,7 @@ public class DSCourseWork {
 								+ e.getMessage());
 						System.exit(0);
 					}
-
+					linecount++;
 				}
 				
 				
@@ -182,7 +187,7 @@ public class DSCourseWork {
 							fields.append(tokens[i]+",");
 							//If quote get the value of id and item
 							if(tokens[0].equalsIgnoreCase("Quote") || tokens[0].equalsIgnoreCase("RFQ")){
-								if(tokens[i].equalsIgnoreCase("id")){currId=tokens[i+1];}
+								if(tokens[i].equalsIgnoreCase("rid")||tokens[i].equalsIgnoreCase("qid")){currId=tokens[i+1];}
 								if(tokens[i].equalsIgnoreCase("item")){currItem=tokens[i+1];}
 							}
 						}else{
@@ -201,14 +206,15 @@ public class DSCourseWork {
 				 /**
 				  * If line or message is a quote, then query the RFQ table to verify its mapping
 				  * */
+				 System.out.println(sql);
 				 if(tokens[0].equalsIgnoreCase("Quote")){ 
-					 String sqlQuery = "SELECT item FROM RFQ WHERE id = "+currId;
+					 String sqlQuery = "SELECT item FROM RFQ WHERE rid = '"+currId+"'";
 					 st = ct.createStatement();
 					 ResultSet rs = st.executeQuery(sqlQuery);
 					 while(rs.next()){
 						 String item = rs.getString("item");
 						 if(!item.equalsIgnoreCase(currItem)){
-							 integrityMsg = "FAILS integrity, the key id with value "+currId+" is bound to "+item+" as an item."; 
+							 integrityMsg = "FAILS integrity, the key rid with value "+currId+" is bound to "+item+" as an item."; 
 						 }
 					 }
 					
@@ -219,13 +225,13 @@ public class DSCourseWork {
 				  * If line or message is RFQ, then query the Quote table to verify its mapping
 				  * */
 				 if(tokens[0].equalsIgnoreCase("RFQ")){ 
-					 String sqlQuery = "SELECT item FROM Quote WHERE id = "+currId;
+					 String sqlQuery = "SELECT item FROM Quote WHERE rid = '"+currId+"'";
 					 st = ct.createStatement();
 					 ResultSet rs = st.executeQuery(sqlQuery);
 					 while(rs.next()){
 						 String item = rs.getString("item");
 						 if(!item.equalsIgnoreCase(currItem)){
-							 integrityMsg = "FAILS integrity, the key id with value "+currId+" is bound to "+item+" as an item."; 
+							 integrityMsg = "FAILS integrity, the key qid with value "+currId+" is bound to "+item+" as an item."; 
 						 }
 					 }
 					
@@ -252,9 +258,9 @@ public class DSCourseWork {
 				}catch (SQLException e) {
 					if(!isNil){//Print this for only non nil column messages.
 						//Without the above condition both nil and no parameter message will be printed
-						printLog("Error", errorHandling(e.getMessage()));
+						//printLog("Error", errorHandling(e.getMessage()));
 					}
-					//printLog("Error", e.getMessage());
+					printLog("Error", e.getMessage());
 				}
 				printLog("=====", "==================END================");
 				//Reset all checkign variables
